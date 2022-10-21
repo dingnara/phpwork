fba2b89166d79a88ff51e4b65a9f2cb010d72db4
Dingnara <s64309010004@ptnicec.ac.th>
2022-10-21 11:50:46 +0700

    ส่งไฟร์Comment ครั้งที่2

---
+++ comment.php
@@ -0,0 +1,14 @@
+<?php
+
+//บรรทัดที่ 6-7 แสดงผลการใช้งาน Comment แบบบรรทัดเดียว
+echo "Solahuding Keuji<br>";
+echo "Information Technology<br>";
+//echo "Pattani<br>";
+#echo "Thailand<br>";
+
+//บรรทัดที่ 10-12 แสดงผลการใช้งาน Comment แบบหลายบรรทัด
+/*echo "94000<br>";
+echo "Asisa<br>";*/
+echo "Universal";
+
+?>
